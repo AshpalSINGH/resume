2656cdedd8990115637cf743002032d0af256479
AshpalSINGH <124709820+AshpalSINGH@users.noreply.github.com>
2023-05-02 23:24:24 +1000

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 import requests
 from streamlit_lottie import st_lottie
 from streamlit_option_menu  import option_menu
+from PIL import Image
 st.set_page_config(page_title="My resume", page_icon=":page_facing_up:",layout="wide")
 
 with st.sidebar:
-    selected=option_menu(menu_title="Main Menu", options=["Home","Contact"],icons=["house","envelope"])
+    selected=option_menu(menu_title="main menu", options=["Home","About Me","Contact"],icons=["house","person","envelope"])
 
 def load_lottie(url):
     r= requests.get(url)
@@ -29,24 +30,58 @@ if selected=="Home":
         c1,c2=st.columns(2)
         with c1:
             st.header("About Me")
-            st.write("With a passion for computer programming and a strong desire to become a successful data analyst, I bring a unique blend of technical aptitude and analytical thinking to any project. I have a deep understanding of programming languages such as Python , as well as experience working with csv files, several data sets and creating data-driven solutions. I am committed to continuous learning and professional development, and am always seeking out new opportunities to challenge myself and enhance my skills.")
+            st.info("With a passion for computer programming and a strong desire to become a successful data analyst, I bring a unique blend of technical aptitude and analytical thinking to any project. I have a deep understanding of programming languages such as Python , as well as experience working with csv files, several data sets and creating data-driven solutions. I am committed to continuous learning and professional development, and am always seeking out new opportunities to challenge myself and enhance my skills.")
+            btn1= st.button("More about Me")
+            if btn1:
+                with st.container():
+                    selected="About Me"
+
+
         with c2:
             st_lottie(lottie_animation,height=300)
+if selected=="About Me":
+    st.write("---")
+    with st.container():
+        c1,c2=st.columns(2)
+        with c1:
+            st.subheader("More About Me")
+            st.write("Name: Ashpal Singh")
+            st.write("Age: 19")
+            st.write("Email: ashpalsingh0909@gmail.com")
+            st.write("Contact No.: +61 0499195301")
+            st.write("Skills: ")
+        c11,c22=st.columns(2)
+        with c11:
+            st.info("Python")
+            st.info("SQL")
+            st.info("Numpy")
+            st.info("Matplotlib")
+            st.info("Streamlit")
+            st.info("Web Scraping")
+
+        with c22:
+            st.info("Microsoft Power Bi")
+            st.info("Data Analytics")
+            st.info("Pandas")
+            st.info("Plotly")
+            st.info("HTML")
+# ------------certifications----------
+
 
 #-----------------contact---------------
-else:
+if selected=="Contact":
     with st.container():
         st.write("---")
         st.header("Get in touch with Me!")
 
         contact_form="""
         <form action="https://formsubmit.co/ashpalsingh0909@gmail.com" method="POST">
-            <input type="hidden" name="_capcha" value="False" required>
-            <input type="text" name="name" placeholder="Your name here" required>
-            <input type="email" name="email" placeholder="Your email here" required>
-            <input type="text" name="message" placeholder="Your message here" required>
-            <button type="submit">Submit/button>
-        </form>"""
+        <input type="text" name="name" placeholder="Enter your name" required>
+        <input type="email" name="email" placeholder="Enter your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
 
     c1,c2=st.columns(2)
     with c1:
